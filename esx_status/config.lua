ad06f31e5bccb0c602dbf3b12370bcd0b8ecc8b8
rh <87755064+reyyghi@users.noreply.github.com>
2022-05-27 09:48:17 +0000

    Add files via upload

---
+++ esx_status/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.StatusMax      = 1000000
+Config.TickTime       = 500
+Config.UpdateInterval = 30000
+Config.Display        = false	-- Enable the esx_status bars (disable if you are using another HUD)
